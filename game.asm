d21eeeb3e88bc2384c17cbd8b5b6911d92c15791
Chun Kang Lu <chunkanglu@gmail.com>
2023-03-18 20:19:16 -0400

    Resize display, add restart

--- game.asm
+++ game.asm
@@ -8,8 +8,8 @@
 # Bitmap Display Configuration: 
 # - Unit width in pixels: 8 (update this as needed)  
 # - Unit height in pixels: 8 (update this as needed) 
-# - Display width in pixels: 256 (update this as needed) 
-# - Display height in pixels: 256 (update this as needed) 
+# - Display width in pixels: 512 (update this as needed) 
+# - Display height in pixels: 512 (update this as needed) 
 # - Base Address for Display: 0x10008000 ($gp) 
 # 
 # Which milestones have been reached in this submission? 
@@ -39,7 +39,7 @@
 # ----------------------------------------
 
 .eqv  	DISP_BASE  	0x10008000
-.eqv	DISP_SIZE	1024 				# 32 units * 32 units = 1024
+.eqv	DISP_SIZE	4096 				# 64 units * 64 units = 4096
 .eqv	DISP_W		0x00000080 # unused
 .eqv	DISP_H		0x00000080 # unused
 .eqv	PLAY_BASE	0x10008000 # unused
@@ -49,16 +49,17 @@
 .eqv 	SLP_T		40			# Sleep time
 
 # Colours
-.eqv	P_COL_P		0xff0000 	# Red
-.eqv	P_COL_S		0x81a0a9 	# Light Blue
-.eqv	P_COL_T		0x585858	# Gray
-.eqv	BG			0x000000	# Black
+.eqv	RED			0xff0000
+.eqv	D_RED		0x7f2525
+.eqv	CYAN		0x81a0a9 	
+.eqv	GRAY		0x585858	
+.eqv	BLACK		0x000000	
 
 # ----------------------------------------
 # Stored
 # ----------------------------------------
 .data
-PLAYER_XY:			.word 0, 0 			# x, y where 0 <= x <= 28, 0 <= y <= 27
+PLAYER_XY:			.word 2, 2 			# x, y where 0 <= x <= 60, 0 <= y <= 59
 							   			# This marks top-left unit of 4 by 5 player
 PLAYER_LR:			.word 0				# 0 stationary, 1,left, 2 right
 PLAYER_UD:			.word 0 			# 0 stationary, 1 up (jump), 2 down (falling from jump or ledge)
@@ -75,7 +76,7 @@ main:
 		li $s0, DISP_BASE 	# $s0 stores the base address for display
 		# MIGHT NOT NEED TO BE USED
 		
-		li $t4, 0 # iter
+		li $t0, 0 # iter
 		
 		la $s1, PLAYER_XY
 		la $s2, PLAYER_LR
@@ -83,8 +84,8 @@ main:
 		
 		jal clear_screen
 		
-loop:	beq $t4, 100, exit # TODO: temporary exit condition
-		addi $t4, $t4, 1
+loop:	beq $t0, 100, exit # TODO: temporary exit condition
+		addi $t0, $t0, 1
 		
 		
 		lw $t2, 0($s1) 				# Load player X-coord
@@ -92,7 +93,7 @@ loop:	beq $t4, 100, exit # TODO: temporary exit condition
 		
 		# Calculate unit location from coords
 		sll $t2, $t2, 2				# $t2 = $t2 * 4	(Right $t2 units)
-		sll $t3, $t3, 7				# $t3 = $t3 * 128 (Down $t3 rows)
+		sll $t3, $t3, 8				# $t3 = $t3 * 64 * 4 (Down $t3 rows)
 		addi $t1, $t2, DISP_BASE	# $t1 = base + x-offset
 		add $t1, $t1, $t3			# $t1 = updated + y-offset
 		
@@ -120,6 +121,7 @@ check_keypress:
 		lw $t3, 4($s1) 				# Reload player Y-coord
 
 		lw $t8, 4($t9) 				# this assumes $t9 is set to 0xfff0000 from before 
+		beq $t8, 0x70, restart   	# ASCII code of 'p' is 0x70
 		beq $t8, 0x61, key_left   	# ASCII code of 'a' is 0x61
 		beq $t8, 0x64, key_right   	# ASCII code of 'd' is 0x64
 		
@@ -127,10 +129,23 @@ check_keypress:
 		sw $zero, 0($s2)				# Set PLAYER_LR to 0
 		
 		j update_player
+		
+restart:
+		# TODO: go to first stage
+		li $t2, 2 					# TODO: replace with start pos
+		li $t3, 2
+		sw $t2, 0($s1)
+		sw $t3, 4($s1)
+		# TODO: clear score & reset health
+		# TODO: reset all iteration variables for tracking cycles (walk anim...)
+		# TODO: reset other player states
+		
+		j main
+		
 
 # t1: player position, t2: x-coord | PLAYER_LR, s1: PLAYER_XY
 key_left:
-		beq $t2, 0, update_player  # Do not go right if at right edge of display
+		beq $t2, 0, update_player  # Do not go left if at left edge of display
 		
 		addi $t1, $t1, -4 			# player position Left 1
 		addi $t2, $t2, -1 			# Update PLAYER_XY
@@ -142,7 +157,7 @@ key_left:
 		j update_player
 						
 key_right:
-		beq $t2, 28, update_player  # Do not go right if at right edge of display
+		beq $t2, 60, update_player  # Do not go right if at right edge of display
 		
 		addi $t1, $t1, 4 			# player position Right 1
 		addi $t2, $t2, 1 			# Update PLAYER_XY
@@ -155,7 +170,7 @@ key_right:
 
 # ---- CLEAR SCREEN +
 clear_screen:
-		li $t7, BG
+		li $t7, BLACK
 		li $t8, 0 					# Iterator over screen
 		li $t9, DISP_BASE			# Iter address to clear
 		
@@ -171,16 +186,18 @@ clear_end:
 # ---- CLEAR SCREEN -
  		
 set_player:
-		li $t7, P_COL_P
-		li $t8, P_COL_S
-		li $t9, P_COL_T
+		li $t6, D_RED
+		li $t7, RED
+		li $t8, CYAN
+		li $t9, GRAY
 		
 		j draw_player
 		
 clear_player:
-		li $t7, BG
-		li $t8, BG
-		li $t9, BG
+		li $t6, BLACK
+		li $t7, BLACK
+		li $t8, BLACK
+		li $t9, BLACK
 		
 		j draw_player
 
@@ -189,20 +206,20 @@ draw_player:
 		sw $t7, 4($t1)
 		sw $t7, 8($t1)
 		sw $t7, 12($t1)
-		sw $t9, 128($t1)
-		sw $t7, 132($t1)
-		sw $t8, 136($t1)
-		sw $t8, 140($t1)
-		sw $t9, 256($t1)
+		sw $t9, 256($t1)			# Next row is 256 offset
 		sw $t7, 260($t1)
-		sw $t7, 264($t1)
-		sw $t7, 268($t1)
-		sw $t9, 384($t1)
-		sw $t7, 388($t1)
-		sw $t7, 392($t1)
-		sw $t7, 396($t1)
+		sw $t8, 264($t1)
+		sw $t8, 268($t1)
+		sw $t9, 512($t1)
 		sw $t7, 516($t1)
+		sw $t7, 520($t1)
 		sw $t7, 524($t1)
+		sw $t9, 768($t1)
+		sw $t7, 772($t1)
+		sw $t7, 776($t1)
+		sw $t7, 780($t1)
+		sw $t6, 1028($t1)
+		sw $t6, 1036($t1)
 		
 		jr $ra
 		
